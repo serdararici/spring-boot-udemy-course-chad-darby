5413cc88cf59cd103527e6f7986d441377e3a9d0
Serdar Arici <serdararici3@gmail.com>
2025-12-29 07:17:36 +0300

    Spring boot command line demo.

---
+++ 01-spring-boot-overview/05-command-line-demo/src/main/java/com/example/mycoolapp/FunRestController.java
@@ -0,0 +1,23 @@
+package com.example.mycoolapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is a lucky day!";
+    }
+}
