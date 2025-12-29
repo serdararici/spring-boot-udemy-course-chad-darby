0bfc205fb7fcb56c4511c9ec55734cb31359914f
Serdar Arici <serdararici3@gmail.com>
2025-12-29 12:29:47 +0300

    Spring boot bean-scopes

---
+++ 02-spring-boot-spring-core/07-bean-scopes/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,19 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)  // prototype scope . New object instance for each injection
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practise fast bowling for 15 minutes!!";
+    }
+}
