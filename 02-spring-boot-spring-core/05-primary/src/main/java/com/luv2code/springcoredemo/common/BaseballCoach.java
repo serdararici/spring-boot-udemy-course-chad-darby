02ba82a2d38fa97f3d7de2c09b57ca6ab39ec386
Serdar Arici <serdararici3@gmail.com>
2025-12-29 11:18:58 +0300

    Spring boot primary

---
+++ 02-spring-boot-spring-core/05-primary/src/main/java/com/luv2code/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class BaseballCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Spring 30 minutes in batting practise";
+    }
+}
